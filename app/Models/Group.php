257d07db3f2e89f3c78f307880462c8ca7eba779
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-14 17:10:28 +0330

    feat: db diagram implement.

---
+++ app/Models/Group.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'desc',
+        'owner_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'group_id');
+    }
+}
