4c2bce94cd1dc0743bbdab0023d05d8ef001a228
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-15 17:24:55 +0330

    chore: add create new group icon.

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,7 +1,7 @@
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { Hash } from 'lucide-react';
+import { Hash, Plus } from 'lucide-react';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
@@ -10,7 +10,10 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
 
     return (
         <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Groups</SidebarGroupLabel>
+            <SidebarGroupLabel className='flex justify-between'>
+                Groups
+                <Plus className='cursor-pointer'/>
+            </SidebarGroupLabel>
             <SidebarMenu>
                 {groups.map((item) => (
                     <SidebarMenuItem key={item.id}>
