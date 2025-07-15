83bb585322c9aa5fd76ed555eb5c783973feb3ac
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-15 17:48:28 +0330

    feat(ui): show edit group icon on hover in sidebar

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,26 +1,29 @@
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { Hash, Plus } from 'lucide-react';
+import { Hash, Plus, Edit } from 'lucide-react';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
-    const {groups} = page.props
+    const { groups } = page.props
 
 
     return (
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel className='flex justify-between'>
                 Groups
-                <Plus className='cursor-pointer'/>
+                <Plus className='cursor-pointer' />
             </SidebarGroupLabel>
             <SidebarMenu>
                 {groups.map((item) => (
                     <SidebarMenuItem key={item.id}>
-                        <SidebarMenuButton className='text-gray-400' asChild isActive={page.url.startsWith(item.id)} tooltip={{ children: item.name }}>
-                            <Link href={item.id} prefetch>
-                                <Hash/>
-                                <span>{item.name}</span>
+                        <SidebarMenuButton className='text-gray-400 group' asChild isActive={page.url.startsWith(item.id)} tooltip={{ children: item.name }}>
+                            <Link href={item.id} prefetch className='flex justify-between hover:[&>svg]:block'>
+                                <span className='flex'>
+                                    <Hash className='w-4 mr-2'/>
+                                    {item.name}
+                                </span>
+                                <Edit className='justify-self-end hidden' />
                             </Link>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
