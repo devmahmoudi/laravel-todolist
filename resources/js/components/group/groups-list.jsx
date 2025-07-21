8df7de6653efd205305923a09c0d0cff0aa6ac4c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-21 17:38:02 +0330

    feat: display input for create new group when click on plus icon.

--- resources/js/components/group/groups-list.jsx
+++ resources/js/components/group/groups-list.jsx
@@ -1,13 +1,19 @@
 import { usePage } from '@inertiajs/react';
 import GroupItem from '@/components/group/group-item'
+import CreateGroup from '@/components/group/create-group'
 
 
-const GroupsList = () => {
+const GroupsList = ({displayCreateGroupInput}) => {
     const page = usePage();
     const { groups } = page.props
 
     return (
         <>
+            {
+                (displayCreateGroupInput && (
+                    <CreateGroup/>
+                ))
+            }
             {groups.map((item) => (
                 <GroupItem item={item} key={item.id} />
             ))}
--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -2,16 +2,19 @@ import { SidebarGroup, SidebarGroupLabel, SidebarMenu } from '@/components/ui/si
 import { type NavItem } from '@/types';
 import { Plus } from 'lucide-react';
 import GroupsList from '@/components/group/groups-list'
+import { useState } from 'react';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
+    const [displayCreateGroupInput, setDisplayCreateGroupInput] = useState(false)
+
     return (
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel className='flex justify-between'>
                 Groups
-                <Plus className='cursor-pointer' />
+                <Plus className='cursor-pointer' onClick={() => setDisplayCreateGroupInput(!displayCreateGroupInput)}/>
             </SidebarGroupLabel>
             <SidebarMenu>
-                <GroupsList/>
+                <GroupsList displayCreateGroupInput={displayCreateGroupInput}/>
             </SidebarMenu>
         </SidebarGroup>
     );
