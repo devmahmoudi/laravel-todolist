778d05e28c6f0e06acf06f31a63e4210c5e858b1
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-25 17:16:26 +0330

    feat(toast): display toast messages in top-center position

--- resources/js/components/ui/sonner.tsx
+++ resources/js/components/ui/sonner.tsx
@@ -1,8 +1,31 @@
+import { usePage } from "@inertiajs/react"
 import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { useEffect } from "react"
+import { Toaster as Sonner, ToasterProps, toast } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
+  const { toasts } = usePage().props
+
+  useEffect(() => {
+    Object.entries(toasts).map(([key, value]) => {
+      if (value)
+        switch (key) {
+          case "success":
+            toast.success(value)
+            break
+          case "info":
+            toast.info(value)
+            break
+          case "warning":
+            toast.warning(value)
+            break
+          case "error":
+            toast.error(value)
+            break
+        }
+    })
+  }, [toasts])
 
   return (
     <Sonner
