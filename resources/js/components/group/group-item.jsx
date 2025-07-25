07aa45d590591b6948c0629816b9e28c39ba233e
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-25 12:51:42 +0330

    feat: added group delete option (#20)
    * chore: npx shadcn@latest add dropdown-menu
    
    * chore: remove console.log.
    
    * refactor: Group edit button moved to the DropDownMenu component
    
    * feat: added group delete controller action
    
    * feat: added delete group item in the dropdown menu
    
    * feat: subbmit a request to delete desired group
    
    * feat: npx shadcn@latest add alert-dialog
    
    * feat: added group item confirmation delete dialog

--- resources/js/components/group/group-item.jsx
+++ resources/js/components/group/group-item.jsx
@@ -1,45 +1,117 @@
 import { SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { Link, usePage } from '@inertiajs/react';
-import { Hash, Plus, Edit } from 'lucide-react';
+import { Link, router, usePage } from '@inertiajs/react';
+import { Hash, EllipsisVertical, Plus, Edit, Trash } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 import { preventNavigate } from '@/lib/utils';
 import EditGroup from '@/components/group/edit-group'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 const GroupItem = ({ item }) => {
     const page = usePage();
     const [enableEditGroup, setEnableEditGroup] = useState(false)
+    const [displayDeleteDialog, setDisplayDeleteDialog] = useState(false)
 
     const handleEditIconClick = (e) => {
-        preventNavigate(e)
-
         setEnableEditGroup(true)
     }
 
+    const handleDeleteItem = () => {
+        router.delete(route('group.destroy', item.id))
+    }
+
     return (
-        <SidebarMenuItem key={item.id}>
-            <SidebarMenuButton className='text-gray-400 group' asChild isActive={page.url.startsWith(item.id)} tooltip={{ children: item.name }}>
-                <Link href={item.id} prefetch className='flex justify-between hover:[&>svg]:block'>
-                    <span className='flex align-middle'>
-                        <Hash className='w-4 mr-2' />
-                        {
-                            enableEditGroup ?
-                                (
-                                    <EditGroup group={item} onSaved={() => {
-                                        setEnableEditGroup(false)
-                                    }}/>
-                                ) :
-                                (
-                                    <>
-                                        <span>{item.name}</span>
-                                    </>
-                                )
-                        }
-                    </span>
-                    <Edit className='justify-self-end hidden py-3 pl-2 box-content' onClick={handleEditIconClick} />
-                </Link>
-            </SidebarMenuButton>
-        </SidebarMenuItem>
+        <>
+            <SidebarMenuItem key={item.id}>
+                <SidebarMenuButton className='text-gray-400 group' asChild isActive={page.url.startsWith(item.id)} tooltip={{ children: item.name }}>
+                    <div className='flex justify-between hover:[&>svg]:block'>
+                        <>
+                            <span className='flex align-middle'>
+                                <Hash className='w-4 mr-2' />
+                                {
+                                    enableEditGroup ?
+                                        (
+                                            <EditGroup group={item} onSaved={() => {
+                                                setEnableEditGroup(false)
+                                            }} />
+                                        ) :
+                                        (
+                                            <>
+                                                <span>{item.name}</span>
+                                            </>
+                                        )
+                                }
+                            </span>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <EllipsisVertical className='w-4 h-4 py-1 box-content' />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuItem
+                                        className='cursor-pointer'
+                                        onClick={(e) => {
+                                            setTimeout(() => {
+                                                handleEditIconClick(e);
+                                            }, 200);
+                                        }}
+                                    >
+                                        <Edit />
+                                        Edit
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem
+                                        className='cursor-pointer'
+                                        onClick={(e) => {
+                                            setTimeout(() => {
+                                                setDisplayDeleteDialog(true)
+                                            }, 200);
+                                        }}
+                                    >
+                                        <Trash />
+                                        Delete
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu></>
+                    </div>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            {/* BEGIN: Deletation Alert Dialog */}
+            <AlertDialog open={displayDeleteDialog}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete the group and its todos.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel className='cursor-pointer' onClick={() => setDisplayDeleteDialog(false)}>Cancel</AlertDialogCancel>
+                        <AlertDialogAction className='cursor-pointer' onClick={handleDeleteItem}>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+            {/* END: Deletation Alert Dialog */}
+        </>
+
     )
 }
 
