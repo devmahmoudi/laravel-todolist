d901658ca75eda2985ab73a056899613d1ed7b4b
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-07-27 16:38:38 +0330

    feat(TodoIndex): render todo items table

--- resources/js/pages/todo/todo-index.tsx
+++ resources/js/pages/todo/todo-index.tsx
@@ -2,12 +2,21 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 
 
 
 const TodoIndex = () => {
-    const {group, todos} = usePage().props
+    const { group, todo } = usePage().props
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
@@ -16,14 +25,31 @@ const TodoIndex = () => {
         },
     ];
 
-    console.log(todos);
-    
-
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-               
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Title</TableHead>
+                            <TableHead>Description</TableHead>
+                            <TableHead>Created At</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {
+                            todo.map((item) => (
+                                <TableRow key={item.id}>
+                                    <TableCell className="font-medium truncate max-w-[200px]">{item.title}</TableCell>
+                                    <TableCell className="truncate max-w-[400px]">{item.description}</TableCell>
+                                    <TableCell>{item.created_at}</TableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
             </div>
         </AppLayout>
     );
