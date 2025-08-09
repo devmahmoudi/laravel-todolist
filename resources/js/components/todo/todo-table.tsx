36e21cdc0f465977f6d4ca8f4ba371e567cd830a
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-09 18:14:26 +0330

    feat(TodoTable): enhance todo table with dialog functionality for creating, editing, and deleting todos

--- resources/js/components/todo/todo-table.tsx
+++ resources/js/components/todo/todo-table.tsx
@@ -1,4 +1,4 @@
-import { Todo } from '@/types';
+import { Group, Todo } from '@/types';
 import {
     Table,
     TableBody,
@@ -13,60 +13,81 @@ import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
 import { Link } from '@inertiajs/react';
 import { dateFnsFormat } from '@/lib/utils';
+import { useState } from 'react';
+import CreateTodoDialog from '@/components/todo/create-todo-dialog'
+import DeleteTodoConfirmationDialog from '@/components/todo/delete-todo-confirmation-dialog';
+import EditTodoDialog from '@/components/todo/edit-todo-dialog';
+import { router } from '@inertiajs/react';
 
 interface TodoTableProps {
     todos: Todo[];
+    group: Group;
     onClickNewTodoButton: () => void;
     onDeleteTodo: () => void;
     onEditTodo: () => void;
     onShowDetail: () => void;
 }
 
-const TodoTable = ({ todos, onClickNewTodoButton, onDeleteTodo, onEditTodo, onShowDetail }: TodoTableProps) => {
+const TodoTable = ({ todos, group }: TodoTableProps) => {
+    const [showCreateDialog, setShowCreateDialog] = useState(false)
+    const [todoToDelete, setTodoToDelete] = useState(null)
+    const [todoToEdit, setTodoToEdit] = useState(null)
+
     return (
-        <Table>
-            <TableCaption>
-                <div className='grid gap-3'>
-                    {!todos.length && (
-                        <p>There are no todos in this group</p>
-                    )}
-                    <Separator className='border-white' />
-                    <div>
-                        <Button className='cursor-pointer' size={'sm'} onClick={() => onClickNewTodoButton()}>
-                            <Plus />
-                            <span>Create new todo</span>
-                        </Button>
+        <>
+            {/* CREATE NEW TODO DIALOG */}
+            <CreateTodoDialog open={showCreateDialog} setOpen={setShowCreateDialog} groupId={group.id} />
+
+            {/* DELETE TODO CONFIRMATION DIALOG */}
+            {todoToDelete ? <DeleteTodoConfirmationDialog todo={todoToDelete} onConfirm={() => router.delete(route('todo.delete', todoToDelete.id))} onClose={() => setTodoToDelete(null)} /> : null}
+
+            {/* EDIT TODO DIALOG */}
+            {todoToEdit ? <EditTodoDialog todo={todoToEdit} onClose={() => setTodoToEdit(null)} /> : null}
+
+            <Table>
+                <TableCaption>
+                    <div className='grid gap-3'>
+                        {!todos.length && (
+                            <p>There are no todos in this group</p>
+                        )}
+                        <Separator className='border-white' />
+                        <div>
+                            <Button className='cursor-pointer' size={'sm'} onClick={() => setShowCreateDialog(true)}>
+                                <Plus />
+                                <span>Create new todo</span>
+                            </Button>
+                        </div>
                     </div>
-                </div>
-            </TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">Title</TableHead>
-                    <TableHead>Description</TableHead>
-                    <TableHead>Created At</TableHead>
-                    <TableHead>Actions</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {
-                    todos.map((item) => (
-                        <TableRow key={item.id}>
-                            <TableCell className="font-medium truncate max-w-[200px] cursor-pointer">
-                                <Link href={route('todo.show', item.id)}>
-                                    {item.title}
-                                </Link>
-                            </TableCell>
-                            <TableCell className="truncate max-w-[400px]">{item.description}</TableCell>
-                            <TableCell>{dateFnsFormat(item.created_at, 'PPpp')}</TableCell>
-                            <TableCell>
-                                <Button size={'sm'} variant={'link'} className='text-red-400 cursor-pointer' onClick={() => onDeleteTodo(item)}>Delete</Button>
-                                <Button size={'sm'} variant={'link'} className='text-blue-400 cursor-pointer' onClick={() => onEditTodo(item)}>Edit</Button>
-                            </TableCell>
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-        </Table>
+                </TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Title</TableHead>
+                        <TableHead>Description</TableHead>
+                        <TableHead>Created At</TableHead>
+                        <TableHead>Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        todos.map((item) => (
+                            <TableRow key={item.id}>
+                                <TableCell className="font-medium truncate max-w-[200px] cursor-pointer">
+                                    <Link href={route('todo.show', item.id)}>
+                                        {item.title}
+                                    </Link>
+                                </TableCell>
+                                <TableCell className="truncate max-w-[400px]">{item.description}</TableCell>
+                                <TableCell>{dateFnsFormat(item.created_at, 'PPpp')}</TableCell>
+                                <TableCell>
+                                    <Button size={'sm'} variant={'link'} className='text-red-400 cursor-pointer' onClick={() => setTodoToDelete(item)}>Delete</Button>
+                                    <Button size={'sm'} variant={'link'} className='text-blue-400 cursor-pointer' onClick={() => setTodoToEdit(item)}>Edit</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </>
     )
 }
 
--- resources/js/pages/todo/todo-index.tsx
+++ resources/js/pages/todo/todo-index.tsx
@@ -1,18 +1,12 @@
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, router, usePage } from '@inertiajs/react';
-import { useState } from 'react';
-import CreateTodoDialog from '@/components/todo/create-todo-dialog'
-import DeleteTodoConfirmationDialog from '@/components/todo/delete-todo-confirmation-dialog';
-import EditTodoDialog from '@/components/todo/edit-todo-dialog';
+import { Head, usePage } from '@inertiajs/react';
 import TodoTable from '@/components/todo/todo-table';
 
 
 const TodoIndex = () => {
     const { group, todos } = usePage().props
-    const [showCreateDialog, setShowCreateDialog] = useState(false)
-    const [todoToDelete, setTodoToDelete] = useState(null)
-    const [todoToEdit, setTodoToEdit] = useState(null)
+    
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
@@ -25,18 +19,9 @@ const TodoIndex = () => {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={`${group.name} todos`} />
 
-            {/* CREATE NEW TODO DIALOG */}
-            <CreateTodoDialog open={showCreateDialog} setOpen={setShowCreateDialog} groupId={group.id} />
-
-            {/* DELETE TODO CONFIRMATION DIALOG */}
-            {todoToDelete ? <DeleteTodoConfirmationDialog todo={todoToDelete} onConfirm={() => router.delete(route('todo.delete', todoToDelete.id))} onClose={() => setTodoToDelete(null)} /> : null}
-
-            {/* EDIT TODO DIALOG */}
-            {todoToEdit ? <EditTodoDialog todo={todoToEdit} onClose={() => setTodoToEdit(null)} /> : null}
-
             {/* TODOS TABLE */}
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <TodoTable todos={todos} onClickNewTodoButton={() => setShowCreateDialog(true)} onDeleteTodo={setTodoToDelete} onEditTodo={setTodoToEdit} onShowDetail={setShowTodoDetail}/>
+                <TodoTable todos={todos} group={group}/>
             </div>
         </AppLayout>
     );
