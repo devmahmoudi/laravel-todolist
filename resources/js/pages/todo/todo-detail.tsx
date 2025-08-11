0a7f2df017827616dd553fa173b2b1093bffebd3
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-08-11 20:20:05 +0330

    feat(breadcrumbs): enhance breadcrumb structure in TodoDetail to include ellipsis for deeper ancestors

--- resources/js/pages/todo/todo-detail.tsx
+++ resources/js/pages/todo/todo-detail.tsx
@@ -1,6 +1,7 @@
 import TodoTable from '@/components/todo/todo-table';
+import { BreadcrumbEllipsis } from '@/components/ui/breadcrumb';
 import AppLayout from '@/layouts/app-layout';
-import { Todo, type BreadcrumbItem } from '@/types';
+import { DropdownBreadcrumbItem, SimpleBreadcrumbItem, BreadcrumbItem, Todo } from '@/types';
 import { Head, Link, router, usePage } from '@inertiajs/react';
 import { Separator } from '@radix-ui/react-separator';
 
@@ -11,13 +12,39 @@ const TodoDetail = ({ todo, ancestors }: { todo: Todo, ancestors: Todo[] }) => {
             title: `# ${todo.group?.name || 'Group'}`,
             href: route('group.todo', todo.group?.id),
         }
-    ].concat(ancestors.map(item => ({
-        title: `${item.title}`,
-        href: route('todo.show', item.id)
-    }))).concat([{
+    ];
+
+
+    if (ancestors.length > 0) {
+        // Add deeper ancestors as ellipsis item in the breadcrumb
+        if (ancestors.length > 1) {
+            const deeperAncestors = ancestors.slice(0, (ancestors.length - 1))
+
+            breadcrumbs.push({
+                trigger: <BreadcrumbEllipsis />,
+                items: deeperAncestors.map((ancestor) => {
+                    return {
+                        title: ancestor.title,
+                        href: route('todo.show', ancestor.id)
+                    }
+
+                })
+            })
+        }
+
+        // add current todo's parent to the breadcrumb items
+        const parent = ancestors.at(-1)
+        breadcrumbs.push({
+            title: parent.title,
+            href: route('todo.show', parent.id)
+        })
+    }
+
+    // add current todo as latest the breadcrumb item
+    breadcrumbs.push({
         title: `${todo.title}`,
         href: route('todo.show', todo.id),
-    }]);
+    })
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
