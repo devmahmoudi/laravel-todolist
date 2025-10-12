b05c986098c8b7b4d88fb01dd199c2be54e9e8f3
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-12 10:58:49 +0330

    feat(DatabaseSeeder): update seeder to create a mix of completed and incomplete todos for each group

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,8 +26,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(5)->create()->each(function ($user) {
             $groups = \App\Models\Group::factory(2)->create(['owner_id' => $user->id]);
             $groups->each(function ($group) {
-                // Each group gets 3 todos
-                \App\Models\Todo::factory(3)->create(['group_id' => $group->id]);
+                // Each group gets 3 todos - mix of completed and incomplete
+                \App\Models\Todo::factory(2)->incomplete()->create(['group_id' => $group->id]);
+                \App\Models\Todo::factory(1)->completed()->create(['group_id' => $group->id]);
             });
         });
     }
