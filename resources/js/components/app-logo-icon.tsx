b773a8239b181b3f2c7883a047b72463ca0cb015
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-08 07:48:05 +0330

    chore: remove npm packages, inertia page and components resources and etc

--- resources/js/components/app-logo-icon.tsx
+++
@@ -1,8 +0,0 @@
-import { SVGAttributes } from 'react';
-import { ListTodo } from 'lucide-react';
-
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <ListTodo className='text-black'/>
-    );
-}
